d5f329a569b4f3e51d4625544d39f3d7d56f216f
Happy <aleko_stastlivetsa@yahoo.com>
2023-08-19 18:35:02 +0300

    New and Updates

---
+++ 04_OOP/01_First_Steps_in_OOP/Exercises/03_employee.py
@@ -0,0 +1,53 @@
+class Employee:
+    def __init__(self, id: int, first_name: str, last_name: str, salary: int):
+        self.id_mun = id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.salary = salary
+
+    def get_full_name(self) -> str:
+        return f"{self.first_name} {self.last_name}"
+
+    def raise_salary(self, amount: int):
+        self.salary += amount
+        return self.salary
+
+    def get_annual_salary(self):
+        return self.salary * 12
+
+
+employee = Employee(744423129, "John", "Smith", 1000)
+print(employee.get_full_name())
+print(employee.raise_salary(500))
+print(employee.get_annual_salary())
+
+import unittest
+
+
+class Tests(unittest.TestCase):
+    def test_init(self):
+        e = Employee(123, "A", "B", 1500)
+        self.assertEqual(e.id, 123)
+        self.assertEqual(e.first_name, "A")
+        self.assertEqual(e.last_name, "B")
+        self.assertEqual(e.salary, 1500)
+
+    def test_get_full_name(self):
+        e = Employee(3333, "C", "B", 400)
+        res = e.get_full_name()
+        self.assertEqual(res, "C B")
+
+    def test_get_annual_salary(self):
+        e = Employee(234155, "NC", "NFD", 4000)
+        res = e.get_annual_salary()
+        self.assertEqual(res, 48000)
+
+    def test_raise_salary(self):
+        e = Employee(1, "1", "2", 1)
+        res = e.raise_salary(199)
+        self.assertEqual(res, 200)
+        self.assertEqual(e.salary, 200)
+
+
+if __name__ == "__main__":
+    unittest.main()
