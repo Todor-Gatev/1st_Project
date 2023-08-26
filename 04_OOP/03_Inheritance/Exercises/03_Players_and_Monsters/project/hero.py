e44c156db7dfa2980c9241930624af9f2827cfc5
Happy <aleko_stastlivetsa@yahoo.com>
2023-08-26 12:26:54 +0300

    New and Updates

---
+++ 04_OOP/03_Inheritance/Exercises/03_Players_and_Monsters/project/hero.py
@@ -0,0 +1,8 @@
+class Hero:
+    def __init__(self, username: str, level: int):
+        self.username = username
+        self.level = level
+
+    def __str__(self):
+        return (f"{self.username} of type {self.__class__.__name__}"
+                f" has level {self.level}")
