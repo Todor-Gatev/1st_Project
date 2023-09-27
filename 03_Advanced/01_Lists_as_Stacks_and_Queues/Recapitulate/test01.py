bb432c12d0a2df4ad35edb464b58a58c97482e11
Happy <aleko_stastlivetsa@yahoo.com>
2023-09-27 15:24:56 +0300

    New and Updates

--- 03_Advanced/01_Lists_as_Stacks_and_Queues/Recapitulate/test01.py
+++ 03_Advanced/01_Lists_as_Stacks_and_Queues/Recapitulate/test01.py
@@ -6,5 +6,24 @@
 # if isinstance(int(a), int):
 #     print(a)
 
-orders = list("abcdf")
-print("Orders left:", *orders, "end", '.')
+# orders = list("abcdf")
+# print("Orders left:", *orders, "end", '.')
+
+# a = set([1, 2, 3, 4])
+# b = set([3, 4, 5, 6])
+# print(a | b)  # Union -> {1, 2, 3, 4, 5, 6}
+# print(a & b)  # Intersection -> {3, 4}
+# print(a < b)  # Subset -> False
+# print(a > b)  # Superset -> False
+# print(a - b)  # Difference -> {1, 2}
+# print(a ^ b)  # Symmetric Difference -> {1, 2, 5, 6}
+#
+# a.union(b)                 # Equivalent to a | b
+# print(a.union(b))          # {1, 2, 3, 4, 5, 6}
+# print(a)                   # {1, 2, 3, 4}
+# a.intersection(b)          # Equivalent to a & b
+# a.issubset(b)              # Equivalent to a <= b
+# a.issuperset(b)            # Equivalent to a >= b
+# a.difference(b)            # Equivalent to a - b
+# a.symmetric_difference(b)  # Equivalent to a ^ b
+
