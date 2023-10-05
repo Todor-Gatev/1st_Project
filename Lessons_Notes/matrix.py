f0d00fdbff3083aa9a32ec26488f9681871db087
Happy <aleko_stastlivetsa@yahoo.com>
2023-10-05 19:49:32 +0300

    New and Updates

--- Lessons_Notes/matrix.py
+++ Lessons_Notes/matrix.py
@@ -18,21 +18,21 @@
 
 # region sum of primary or secondary diagonal
 
-# n = int(input())
-# matrix = [[int(x) for x in input().split()] for _ in range(n)]
-# primary_diagonal = sum([matrix[i][i] for i in range(n)])
-# secondary_diagonal = sum([matrix[i][n - i - 1] for i in range(n)])
-# print(primary_diagonal)
-# print(secondary_diagonal)
+n = int(input())
+matrix = [[int(x) for x in input().split()] for _ in range(n)]
+primary_diagonal = sum([matrix[i][i] for i in range(n)])
+secondary_diagonal = sum([matrix[i][n - i - 1] for i in range(n)])
+print(primary_diagonal)
+print(secondary_diagonal)
 
 # faster solution
-# primary_diagonal_sum = 0
-# secondary_diagonal_sum = 0
-# for i in range(n):
-#     row = [int(x) for x in input().split()]
-#     primary_diagonal_sum += row[i]
-#     secondary_diagonal_sum += row[n - i - 1]
-# print(primary_diagonal_sum)
-# print(secondary_diagonal_sum)
+primary_diagonal_sum = 0
+secondary_diagonal_sum = 0
+for i in range(n):
+    row = [int(x) for x in input().split()]
+    primary_diagonal_sum += row[i]
+    secondary_diagonal_sum += row[n - i - 1]
+print(primary_diagonal_sum)
+print(secondary_diagonal_sum)
 
 # endregion
