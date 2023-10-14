011163bf8a692a25d10e6f66419a99a0b8deb72e
Happy <aleko_stastlivetsa@yahoo.com>
2023-10-14 17:15:47 +0300

    New and Updates

--- 03_Advanced/05_Functions_Advanced/Lab/06_recursive_power.py
+++ 03_Advanced/05_Functions_Advanced/Lab/06_recursive_power.py
@@ -1,28 +1,28 @@
-def recursive_power(number, power):
-    result = 1
-    if power == 0:
-        return result
-    result = number * recursive_power(number, power - 1)
-    return result
-    # return number ** power
+# def recursive_power(num, power):  # short but not good for debugging
+#     if power == 0:
+#         return 1
+#     return num * recursive_power(num, power - 1)
 
-# def recursive_power(number, power):
-#     if power == 1:
-#         return number
-#     return number * recursive_power(number, power - 1)
+# def recursive_power(number, power):longer but in debug you can see how recursion works
+#     result = 1
+#     if power == 0:
+#         return result
+#     result = number * recursive_power(number, power - 1)
+#     return result
 #     # return number ** power
-
-
-print(recursive_power(2, 3))
-print(recursive_power(2, 0))
-# print(recursive_power(2, 10))
-# print(recursive_power(10, 100))
-
-# def a():
-#     a()
 #
 #
-# a()
+# print(recursive_power(2, 3))
+# print(recursive_power(2, 0))
+# # print(recursive_power(2, 10))
+# # print(recursive_power(10, 100))
+
+def a():  # infinite recursion
+    a()
+
+
+a()  # [Previous line repeated 996 more times]
+# RecursionError: maximum recursion depth exceeded
 
 # def fact(n):
 #     if n == 1:
--- Lessons_Notes/functions_adv_notes.py
+++ Lessons_Notes/functions_adv_notes.py
@@ -1,63 +1,83 @@
-# recursion is time and space consuming - can be used when there is dictionary in dictionary...unknown times
-# recursion - see lab task 6 and recursive_funcs.py
-
-def sum_nums(a, c=5,  *args):  # *args (packing)-> tuple with 0 or more ele-> a: 3 c: 7 args: (12, 19)
-    res = a + c
-    for num in args:
-        res += num
-    return res
-
-
-z = [1, 2, 3]
-sum_nums(3, 7, *z)
-sum_nums(3, 7, 12, 19)
-
-
-def sum_values(a, **kwargs):  # **kwargs -> dict with 0 or more ele-> a: 3  kwargs: {'b': 7, 'c': 12}
-    print(a)
-    print(kwargs)
-
-
-sum_values(3, b=7, c=12)
-sum_values(a=3)
-
-print(**{"name": "George", "town": "Sofia", "age": 20})  # error
-print(*[1, 2, 3])  # OK
-
-
-def get_info(name, age, town):
-    return f"This is {name} from {town} and he is {age} years old"
-
-
-print(get_info(**{"name": "George", "town": "Sofia", "age": 20}))  # **(unpacking) transforms dict to next row
-print(get_info(name="George", town="Sofia", age=20))  # kwargs can read this tipe of info
-print(get_info("George", "Sofia", 20))  # for correct result needs correct sequence
+# # recursion is time and space consuming - can be used when there is dictionary in dictionary...unknown times
+# # recursion - see lab task 6 and recursive_funcs.py
+#
+# def sum_nums(a, c=5,  *args):  # *args (packing)-> tuple with 0 or more ele-> a: 3 c: 7 args: (12, 19)
+#     res = a + c
+#     for num in args:
+#         res += num
+#     return res
+#
+#
+# z = [1, 2, 3]
+# sum_nums(3, 7, *z)
+# sum_nums(3, 7, 12, 19)
+#
+#
+# def sum_values(a, **kwargs):  # **kwargs -> dict with 0 or more ele-> a: 3  kwargs: {'b': 7, 'c': 12}
+#     print(a)
+#     print(kwargs)
+#
+#
+# sum_values(3, b=7, c=12)
+# sum_values(a=3)
+#
+# print(**{"name": "George", "town": "Sofia", "age": 20})  # error
+# print(*[1, 2, 3])  # OK
+#
+#
+# def get_info(name, age, town):
+#     return f"This is {name} from {town} and he is {age} years old"
+#
+#
+# print(get_info(**{"name": "George", "town": "Sofia", "age": 20}))  # **(unpacking) transforms dict to next row
+# print(get_info(name="George", town="Sofia", age=20))  # kwargs can read this tipe of info
+# print(get_info("George", "Sofia", 20))  # for correct result needs correct sequence
 
 # def a(x1, y1):
-#     x = 'x'
-#     print(x)
-#     print(x1)
-#     y = 'p'
+#     x = 'xa'  # not changed on global scope
+#     print(x)  # xa  => changed on local scope
+#     print(x1)  # x  => not changed on global scope
 #
 #     def b():
-#         global y
+#         global y  # y  => changed on global scope
 #         nonlocal y1
-#         y1 = 'y1'
-#         y = 'z'
-#         print(y)
-#         print(y1)
+#         y1 = 'y1b'
+#         y = 'yb'
+#         print(y)  # yb  => changed on global scope
+#         print(y1)  # y1b  => changed on local scope
 #
 #     return b  # if not return b -> b is hidden
 #
 #
-# x = 'a'
-# y = 'b'
+# x = 'x'
+# y = 'y'
 # a(x, y)()  # if b is not hidden, we can indirectly call b
-# res = a(x, y)
-# res()  # if b is not hidden, we can indirectly call b
+# # res = a(x, y)
+# # res()  # if b is not hidden, we can indirectly call b
 # print(x)
 # print(y)
-# # b()  # ERROR
+# # # b()  # ERROR
+
+
+# region Function Returning Function Example
+def calculator(operator):
+    def addition(a, b):
+        return a + b
+
+    def subtraction(a, b):
+        return a - b
+
+    if operator == "+":
+        return addition
+    elif operator == "-":
+        return subtraction
+
+
+operation = calculator("+")
+result = operation(2, 3)
+print(result)
+
+# endregion
 
 
 # def factorial(number):
