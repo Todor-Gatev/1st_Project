d0a2f8693dcf98d2d937ac569a12a24abbf54a12
Happy <aleko_stastlivetsa@yahoo.com>
2023-10-24 15:22:33 +0300

    New and Updates

--- 04_OOP/01_First_Steps_in_OOP/Recapitulate/Exercises/04_cup.py
+++ 04_OOP/01_First_Steps_in_OOP/Recapitulate/Exercises/04_cup.py
@@ -2,5 +2,15 @@ class Cup:
     def __init__(self, size: int, quantity: int):
         self.size = size
         self.quantity = quantity
+    
+    @property
+    def size(self):
 
+        return
+
+    @size.setter
+    def size(self, value):
+        pass
+
+    
     # def fill(self):
