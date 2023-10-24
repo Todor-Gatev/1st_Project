fa234075ff5585c3fc6139bb6d9126410edd8fee
Happy <aleko_stastlivetsa@yahoo.com>
2023-10-24 15:25:39 +0300

    New and Updates

---
+++ 04_OOP/01_First_Steps_in_OOP/Recapitulate/Exercises/06_steam_user.py
@@ -0,0 +1,29 @@
+from typing import List
+
+
+class SteamUser:
+    def __init__(self, username: str, games: List):
+        self.username = username
+        self.games: List[str] = games
+        self.played_hours = 0
+
+    def play(self, game: str, hours):
+        if game in self.games:
+            self.played_hours += hours
+
+            return f"{self.username} is playing {game}"
+        else:
+            return f"{game} is not in library"
+
+    def buy_game(self, game: str):
+        if game in self.games:
+            return f"{game} is already in your library"
+        else:
+            self.games.append(game)
+
+            return f"{self.username} bought {game}"
+
+    def status(self):
+        return f"{self.username} has {len(self.games)} games. Total play time: {self.played_hours}"
+
+
