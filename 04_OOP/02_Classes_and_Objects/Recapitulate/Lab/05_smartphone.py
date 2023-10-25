d1e17d43e355541fa2a13e1c2a82577181dc492a
Happy <aleko_stastlivetsa@yahoo.com>
2023-10-25 18:48:49 +0300

    New and Updates

---
+++ 04_OOP/02_Classes_and_Objects/Recapitulate/Lab/05_smartphone.py
@@ -0,0 +1,34 @@
+from typing import List
+
+
+class Smartphone:
+    def __init__(self, memory: int):
+        self.memory = memory
+        self.is_on = False
+        self.apps: List[str] = []
+
+    def power(self) -> None:
+        self.is_on = not self.is_on
+
+    def install(self, app: str, app_memory: int) -> str:
+        if self.memory < app_memory:
+            return f"Not enough memory to install {app}"
+        elif not self.is_on:
+            return f"Turn on your phone to install {app}"
+        else:
+            self.apps.append(app)
+            self.memory -= app_memory
+
+            return f"Installing {app}"
+
+    def status(self) -> str:
+        return f"Total apps: {len(self.apps)}. Memory left: {self.memory}"
+
+
+smartphone = Smartphone(100)
+print(smartphone.install("Facebook", 60))
+smartphone.power()
+print(smartphone.install("Facebook", 60))
+print(smartphone.install("Messenger", 20))
+print(smartphone.install("Instagram", 40))
+print(smartphone.status())
