b0dfc3298428e1ce391d8dfc9d7aaccabf644631
Happy <aleko_stastlivetsa@yahoo.com>
2023-11-03 13:24:19 +0200

    New and Updates

--- 04_OOP/03_Inheritance/Recapitulate/Lab/test_inh.py
+++ 04_OOP/03_Inheritance/Recapitulate/Lab/test_inh.py
@@ -1,19 +1,56 @@
-class Vehicle:
-    DEFAULT_FUEL_CONSUMPTION: float = 1.25
+# class Vehicle:
+#     DEFAULT_FUEL_CONSUMPTION: float = 1.25
+#
+#     def __init__(self, fuel: float, horse_power: int):
+#         self.fuel = fuel
+#         self.horse_power = horse_power
+#         self.fuel_consumption = self.DEFAULT_FUEL_CONSUMPTION
+#         # self.fuel_consumption = Vehicle.DEFAULT_FUEL_CONSUMPTION
+#
+#
+# # is_long_enough = len(value) > 7
+# # is_upper_char = [x for x in value if x.isupper()]
+# # is_digit_in_password = [x for x in value if x.isdigit()]
+# c = Vehicle(3, 7)
+# print(c.__repr__())
+# print(c.__class__.__name__)
+#
+# a = {'a': 1, 'b': 2, 'c': 's'}
+# print(len(a))
 
-    def __init__(self, fuel: float, horse_power: int):
-        self.fuel = fuel
-        self.horse_power = horse_power
-        self.fuel_consumption = self.DEFAULT_FUEL_CONSUMPTION
-        # self.fuel_consumption = Vehicle.DEFAULT_FUEL_CONSUMPTION
+
+from abc import ABC, abstractmethod
 
 
-# is_long_enough = len(value) > 7
-# is_upper_char = [x for x in value if x.isupper()]
-# is_digit_in_password = [x for x in value if x.isdigit()]
-c = Vehicle(3, 7)
-print(c.__repr__())
-print(c.__class__.__name__)
+class Animal(ABC):
+    def __init__(self, name):
+        self.name = name
 
-a = {'a': 1, 'b': 2, 'c': 's'}
-print(len(a))
+    @abstractmethod
+    def sound(self):
+        raise NotImplementedError("Subclass must implement")
+
+
+#
+class Dog(Animal):
+    def __init__(self, name):
+        super().__init__(name)
+
+    def sound(self):
+        print("Bark!")
+
+
+class Cat(Animal):
+    def __init__(self, name):
+        super().__init__(name)
+
+    def sound(self):
+        print("Meow!")
+
+
+cat = Cat("Willy")
+cat.sound()
+dog = Dog("Willy")
+dog.sound()
+animal = Animal("Willy")  # TypeError: Can't instantiate abstract class Animal with abstract method sound
+# animal.sound()
