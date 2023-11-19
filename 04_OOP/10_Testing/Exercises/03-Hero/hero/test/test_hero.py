f8e5258e353a28236db46e66a8ad4b9931039e6f
Happy <aleko_stastlivetsa@yahoo.com>
2023-11-19 20:20:26 +0200

    New and Updates

--- 04_OOP/10_Testing/Exercises/03-Hero/hero/test/test_hero.py
+++ 04_OOP/10_Testing/Exercises/03-Hero/hero/test/test_hero.py
@@ -5,14 +5,14 @@ from project.hero import Hero
 
 class TestHero(TestCase):
     def setUp(self) -> None:
-        self.hero = Hero("Hero", 1, 3.2, 1.2)
-        self.enemy = Hero("Enemy", 1, 1.6, 0.6)
+        self.hero = Hero("Hero", 1, 100, 100)
+        self.enemy = Hero("Enemy", 1, 50, 50)
 
     def test_correct_initialization(self):
         self.assertEqual("Hero", self.hero.username)
         self.assertEqual(1, self.hero.level)
-        self.assertEqual(3.2, self.hero.health)
-        self.assertEqual(1.2, self.hero.damage)
+        self.assertEqual(100, self.hero.health)
+        self.assertEqual(100, self.hero.damage)
 
     def test_battle_if_hero_is_the_same_as_enemy_exception_expected(self):
         with self.assertRaises(Exception) as ex:
@@ -26,17 +26,51 @@ class TestHero(TestCase):
         with self.assertRaises(ValueError) as ve:
             self.hero.battle(self.enemy)
 
-        self.assertEqual("Your health is lower than or equal to 0. You need to rest", str(ve.exception))
+        expected = "Your health is lower than or equal to 0. You need to rest"
+        self.assertEqual(expected, str(ve.exception))
 
     def test_battle_if_enemy_health_is_zero_or_negative_expected_value_error(self):
         self.enemy.health = 0
-        expected = "You cannot fight Enemy. He needs to rest"
 
         with self.assertRaises(ValueError) as ve:
             self.hero.battle(self.enemy)
 
+        expected = "You cannot fight Enemy. He needs to rest"
         self.assertEqual(expected, str(ve.exception))
-        
+
+    # def test_battle_if_both_hero_and_enemy_health_is_zero_or_negative_after_battle(self):
+
+    def test_battle_decrease_correctly_hero_and_enemy_health_draw_result(self):
+        self.hero.health = 50
+
+        result = self.hero.battle(self.enemy)
+        self.assertEqual("Draw", result)
+
+        self.assertEqual(0, self.hero.health)
+        self.assertEqual(-50, self.enemy.health)
+
+    def test_battle_player_win_proper_increase_level_health_damage(self):
+        result = self.hero.battle(self.enemy)
+
+        self.assertEqual("You win", result)
+        self.assertEqual(2, self.hero.level)
+        self.assertEqual(55, self.hero.health)
+        self.assertEqual(105, self.hero.damage)
+
+    def test_battle_player_lose_proper_increase_level_health_damage(self):
+        result = self.enemy.battle(self.hero)
+
+        self.assertEqual("You lose", result)
+        self.assertEqual(2, self.hero.level)
+        self.assertEqual(55, self.hero.health)
+        self.assertEqual(105, self.hero.damage)
+
+    def test_correct__str__(self):
+        result = f"Hero Hero: 1 lvl\n" \
+               f"Health: 100\n" \
+               f"Damage: 100\n"
+        self.assertEqual(result, str(self.hero))
+
 
 if __name__ == "__main__":
     main()
