de816278f1fd7f9920c0e452eab593aca223c932
Happy <aleko_stastlivetsa@yahoo.com>
2023-11-26 14:47:39 +0200

    New and Updates

--- 04_OOP/Exams/Exams/09-Retake Exam - 18 April 2022/01-Functionality/project/movie_specification/thriller.py
+++ 04_OOP/Exams/Exams/09-Retake Exam - 18 April 2022/01-Functionality/project/movie_specification/thriller.py
@@ -1,9 +1,10 @@
 from project.movie_specification.movie import Movie
 
 
-class Thriller (Movie):
+class Thriller(Movie):
     def __init__(self, title: str, year: int, owner: object, age_restriction: int = 16):
         super().__init__(title, year, owner, age_restriction)
+        self.age_restriction = age_restriction
 
     @property
     def age_restriction(self):
@@ -13,7 +14,6 @@ class Thriller (Movie):
     def age_restriction(self, value):
         if value < 16:
             raise ValueError("Thriller movies must be restricted for audience under 16 years!")
-
         self.__age_restriction = value
 
     def details(self):
