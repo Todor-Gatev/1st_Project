4074d9b8b1a63a2ef8b8125cd0bda072e77d5a0e
Happy <aleko_stastlivetsa@yahoo.com>
2023-11-30 15:29:12 +0200

    New and Updates

---
+++ 04_OOP/Exams/Exams/08-Exam - 14 August 2022/01-Functionality/project (1)/horse_specification/thoroughbred.py
@@ -0,0 +1,11 @@
+from project.horse_specification.horse import Horse
+
+
+class Thoroughbred(Horse):
+
+    @property
+    def max_speed(self):
+        return 140
+
+    def train(self):
+        self.speed = min(self.speed + 3, self.max_speed)
