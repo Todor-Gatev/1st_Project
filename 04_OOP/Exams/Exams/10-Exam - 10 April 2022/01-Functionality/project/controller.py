87f7b85cdf389d439e4b77fceb0322b61a215733
Happy <aleko_stastlivetsa@yahoo.com>
2023-12-06 23:14:20 +0200

    New and Updates

--- 04_OOP/Exams/Exams/10-Exam - 10 April 2022/01-Functionality/project/controller.py
+++ 04_OOP/Exams/Exams/10-Exam - 10 April 2022/01-Functionality/project/controller.py
@@ -1,61 +1,70 @@
 class Controller:
-    VALID_SUSTENANCE_TYPES = ["Food", "Drink"]
+    SUSTENANCE_TYPES = ["Food", "Drink"]
 
     def __init__(self):
         self.players = []
         self.supplies = []
 
-    def add_player(self, *players):
-        new_players = []
+    def __get_player_via_name(self, name):
+        return next(filter(lambda x: x.name == name, self.players), None)
 
-        for p in players:
+    def add_player(self, *args):
+        added_names = []
+
+        for p in args:
             if p not in self.players:
+                added_names.append(p.name)
                 self.players.append(p)
-                new_players.append(p.name)
 
-        return f"Successfully added: {', '.join(new_players)}"
+        return f"Successfully added: {', '.join(added_names)}"
 
-    def add_supply(self, *supplies):
-        self.supplies.extend(supplies)
+    def add_supply(self, *args):
+        [self.supplies.append(s) for s in args]
 
     def sustain(self, player_name: str, sustenance_type: str):
-        player = next(filter(lambda x: x.name == player_name, self.players), None)
+        player = self.__get_player_via_name(player_name)
 
-        if not player or sustenance_type not in Controller.VALID_SUSTENANCE_TYPES:
+        if not player:
             return
 
         if not player.need_sustenance:
             return f"{player_name} have enough stamina."
 
-        for i in range(len(self.supplies) - 1, -1, -1):
-            if self.supplies[i].__class__.__name__ == sustenance_type:
-                sustenance = self.supplies.pop(i)
-                break
-        else:
+        if sustenance_type not in self.SUSTENANCE_TYPES:
+            return
+
+        supply = next(filter(lambda x: x.__class__.__name__ == sustenance_type, reversed(self.supplies)), None)
+
+        if not supply:
             raise Exception(f"There are no {sustenance_type.lower()} supplies left!")
 
-        player.stamina = min(100, player.stamina + sustenance.energy)
+        player.stamina = min(100, player.stamina + supply.energy)
 
-        return f"{player_name} sustained successfully with {sustenance.name}."
+        for idx in range(len(self.supplies) - 1, -1, -1):
+            if self.supplies[idx] == supply:
+                self.supplies.pop(idx)
+                break
+
+        return f"{player_name} sustained successfully with {supply.name}."
 
     def duel(self, first_player_name: str, second_player_name: str):
-        players = sorted(
-            [next(filter(lambda x: x.name == first_player_name, self.players)),
-             next(filter(lambda x: x.name == second_player_name, self.players))],
-            key=lambda x: x.stamina)
+        player1 = self.__get_player_via_name(first_player_name)
+        player2 = self.__get_player_via_name(second_player_name)
 
-        is_zero_stamina = [p.name for p in players if p.stamina == 0]
+        players_with_zero_stamina = [p.name for p in [player1, player2] if p.stamina == 0]
 
-        if is_zero_stamina:
-            return "\n".join(f"Player {name} does not have enough stamina." for name in is_zero_stamina)
+        if players_with_zero_stamina:
+            return "\n".join(f"Player {name} does not have enough stamina." for name in players_with_zero_stamina)
 
-        players[1].stamina -= players[0].stamina / 2
-        players[0].stamina = max(0, players[0].stamina - (players[1].stamina / 2))
+        duelers = sorted([player1, player2], key=lambda p: p.stamina)
 
-        if players[0].stamina < players[1].stamina:
-            return f"Winner: {players[1].name}"
-        else:
-            return f"Winner: {players[0].name}"
+        duelers[1].stamina -= duelers[0].stamina / 2
+
+        duelers[0].stamina = max(0, duelers[0].stamina - duelers[1].stamina / 2)
+
+        duelers = sorted(duelers, key=lambda p: p.stamina)
+
+        return f"Winner: {duelers[1].name}"
 
     def next_day(self):
         for p in self.players:
@@ -64,5 +73,7 @@ class Controller:
             self.sustain(p.name, "Drink")
 
     def __str__(self):
-        res = [*[str(p) for p in self.players], *[s.details() for s in self.supplies]]
-        return "\n".join(res)
+        players_info = [str(p) for p in self.players]
+        supply_info = [s.details() for s in self.supplies]
+
+        return "\n".join([*players_info, *supply_info])
