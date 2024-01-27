484622f0766e8cd2787360c61245629881b0b720
Happy <aleko_stastlivetsa@yahoo.com>
2024-01-27 09:43:57 +0200

    New and Updates

---
+++ 04_OOP/Exams/Exams/01-Retake Exam - 15 August 2023 (1)/01- Structure/project/equipment/elbow_pad.py
@@ -0,0 +1,13 @@
+from project.equipment.base_equipment import BaseEquipment
+
+
+class ElbowPad(BaseEquipment):
+    PROTECTION = 90
+    PRICE = 25.0
+    EQUIPMENT_TYPE = "ElbowPad"
+
+    def __init__(self):
+        super().__init__(self.PROTECTION, self.PRICE)
+
+    def increase_price(self):
+        self.price *= 1.1
